98830ccf5753e0776629bca69a9f16d6c7eb87d5
Francisco Rosal <francisco-rosal98@hotmail.com>
2019-01-16 18:05:53 -0600

    Creacion de clase e interfaz

--- Radio/src/interfaces/RadioInterface.java
+++ Radio/src/interfaces/RadioInterface.java
@@ -1,4 +1,12 @@
 package interfaces;
 
 public interface RadioInterface {
+    void toggle();
+    boolean getState();
+    void changeFrecuency();
+    boolean getFrecuency();
+    void changeStation(boolean up);
+    double getStation();
+    void saveStation(int numButton);
+    void changeStationButton(int numButton);
 }
